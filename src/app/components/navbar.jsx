931adba86657a624753b8dafefe885b5addeca41
Tedy Yohanes <91521891+tedy97123@users.noreply.github.com>
2024-05-17 20:04:06 +0000

    made changes to sections and drop downs in navbar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -228,119 +228,31 @@ export default function Navbar({navClass, navlight}){
                     <ul className={`navigation-menu ${navlight === true ? 'nav-light' : ''}`}>
              
 
-                        <li className={`has-submenu parent-parent-menu-item ${['/product-item'].includes(manu) ? 'active' : ''}`}>
-                            <Link href="#" onClick={()=>setSubManu(setManu === '/product-item' ? '' : '/product-item' )}>Products</Link><span className="menu-arrow"></span>
-
-                            <ul className={`submenu megamenu ${['/product-item'].includes(subManu) ? 'open' : ''}`}>
-                                <li className='ms-0'>
-                                    <ul>
-                                        <li className="megamenu-head ms-0">Product Features</li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Bundle - Upsell</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Hot Stock</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Sticky Add To Cart</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Notify Me (Out Of Stock)</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Ask An Expert</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Variant Image Grouped</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Wishlist</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Trust Badge</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Delivery Information</Link></li>
-                                    </ul>
-                                </li>
-
-                                <li className='ms-0'>
-                                    <ul>
-                                        <li className="megamenu-head ms-0">Product Features</li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Sold In Last</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Color Comparison</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Product Swatches</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Product Select Options</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Pre-Order</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Product 3D, AR Models</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Terms And Conditions Checkbox</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Variant Metafield Description</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Variant Metafield Property</Link></li>
-                                    </ul>
-                                </li>
-
-                                <li className='ms-0'>
-                                    <ul>
-                                        <li className="megamenu-head ms-0">Product Features</li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Product Video</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Size Chart</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Dynamic Checkout</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Product Countdown</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Custom Content</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Custom Options</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Product Combo</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Product Complementary</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Auto Discount</Link></li>
-                                    </ul>
-                                </li>
-                        
-                                <li className='ms-0'>
-                                    <ul>
-                                        <li className="megamenu-head ms-0">Product Features</li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Custom Product Tabs</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Vertical Product Tab</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Horizontal Product Tab</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Social Share</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Related Products</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Recently Viewed Products</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Custom Label</Link></li>
-                                        <li className='ms-0'><Link href="#!" className="sub-menu-item">Local Pick Up</Link></li>
-                                    </ul>
-                                </li>
-                        
-                                <li className='ms-0'>
-                                    <ul>
-                                        <li className="megamenu-head"><Image src='/images/cta.png' width={0} height={0} sizes='100vw' style={{width:'100%' , height:'auto'}} alt=""/></li>
-
-                                        <li className="text-center"><Link href="#!" className="py-2 px-5 inline-block font-medium tracking-wide align-middle duration-500 text-base text-center bg-orange-500/10 text-blue-500 rounded-md me-2 mt-2"><i className="mdi mdi-cart-outline"></i> Shop Now</Link></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-
                             <li className={`has-submenu parent-parent-menu-item ${['/shop-grid', '/shop-grid-left-sidebar', '/shop-grid-right-sidebar','/shop-list','/shop-list-left-sidebar','/shop-list-right-sidebar','/product-detail-one','/product-detail-two','/product-detail-three','/shop-cart','/shop-checkout','/our-store','/brands','/compare-product','/recently-viewed-product','/shop-item','/list-item','/detail-item','/grid-item'].includes(manu) ? 'active' : '' }`}>
-                                <Link href="#" onClick={() => setSubManu(subManu === '/shop-item' ? '' : '/shop-item')}> Shop </Link><span className="menu-arrow"></span>
+                                <Link href="#" onClick={() => setSubManu(subManu === '/shop-item' ? '' : '/shop-item')}> Mens </Link><span className="menu-arrow"></span>
                                 <ul className={`submenu ${['/shop-grid', '/shop-grid-left-sidebar', '/shop-grid-right-sidebar','/shop-list','/shop-list-left-sidebar','/shop-list-right-sidebar','/product-detail-one','/product-detail-two','/product-detail-three','/shop-cart','/shop-checkout','/our-store','/brands','/compare-product','/recently-viewed-product','/shop-item','/list-item','/detail-item','/grid-item'].includes(subManu) ? 'open' : ''}`}>
                                     <li className={`has-submenu parent-menu-item ms-0 ${['/shop-grid','/shop-grid-left-sidebar','/shop-grid-right-sidebar','/grid-item'].includes(manu) ? 'active' : ''}`}>
-                                        <Link href="#" onClick={() => setSubManu(setManu === '/grid-item' ? '' : '/grid-item')}> Shop Grid </Link><span className="submenu-arrow"></span>
-                                        <ul className={`submenu ${['/shop-grid','/shop-grid-left-sidebar','/shop-grid-right-sidebar','/grid-item'].includes(subManu) ? 'open' : ''}`}>
-                                            <li className={`ms-0 ${manu === '/shop-grid' ? 'active' : ''}`}><Link href="/shop-grid" className="sub-menu-item">Shop Grid</Link></li>
-                                            <li className={`ms-0 ${manu === '/shop-grid-left-sidebar' ? 'active' : ''}`}><Link href="/shop-grid-left-sidebar" className="sub-menu-item">Grid Left Sidebar</Link></li>
-                                            <li className={`ms-0 ${manu === '/shop-grid-right-sidebar' ? 'active' : ''}`}><Link href="/shop-grid-right-sidebar" className="sub-menu-item">Grid Right Sidebar</Link></li>
-                                        </ul>  
+                                       <Link href="#" onClick={() => setSubManu(setManu === '/grid-item' ? '' : '/grid-item')}> Tops </Link>
+                                         <Link href="#" onClick={() => setSubManu(setManu === '/grid-item' ? '' : '/grid-item')}> Jeans </Link>
+                                         <Link href="#" onClick={() => setSubManu(setManu === '/grid-item' ? '' : '/grid-item')}> Pants </Link>
                                     </li>
-
-                                    <li className={`has-submenu parent-menu-item ms-0 ${['/shop-list','/shop-list-left-sidebar','/shop-list-right-sidebar','/list-item'].includes(manu) ? 'active' : ''}`}>
-                                        <Link href="#" onClick={() => setSubManu(setManu === '/list-item' ? '' : '/list-item')}> Shop List </Link><span className="submenu-arrow"></span>
-                                        <ul className={`submenu ${['/shop-list','/shop-list-left-sidebar','/shop-list-right-sidebar','/list-item'].includes(subManu) ? 'open' : ''}`}>
-                                            <li className={`ms-0 ${manu === '/shop-list' ? 'active' : ''}`}><Link href="/shop-list" className="sub-menu-item">Shop List</Link></li>
-                                            <li className={`ms-0 ${manu === '/shop-list-left-sidebar' ? 'active' : ''}`}><Link href="/shop-list-left-sidebar" className="sub-menu-item">List Left Sidebar</Link></li>
-                                            <li className={`ms-0 ${manu === '/shop-list-right-sidebar' ? 'active' : ''}`}><Link href="/shop-list-right-sidebar" className="sub-menu-item">List Right Sidebar</Link></li>
-                                        </ul>  
-                                    </li>
-
-                                    <li className={`has-submenu parent-menu-item ms-0 ${['/product-detail-one','/product-detail-two','/product-detail-three','/detail-item'].includes(manu) ? 'active' : ''}`}>
-                                        <Link href="#" onClick={() => setSubManu(setManu === '/detail-item' ? '' : '/detail-item')}> Shop Detail </Link><span className="submenu-arrow"></span>
-                                        <ul className={`submenu ${['/product-detail-one','/product-detail-two','/product-detail-three','/detail-item'].includes(subManu) ? 'open' : ''}`}>
-                                            <li className={`ms-0 ${manu === '/product-detail-one' ? 'active' : ''}`}><Link href="/product-detail-one" className="sub-menu-item">Product Detail One</Link></li>
-                                            <li className={`ms-0 ${manu === '/product-detail-two' ? 'active' : ''}`}><Link href="/product-detail-two" className="sub-menu-item">Product Detail Two</Link></li>
-                                            <li className={`ms-0 ${manu === '/product-detail-three' ? 'active' : ''}`}><Link href="/product-detail-three" className="sub-menu-item">Product Detail Three</Link></li>
-                                        </ul>  
-                                    </li>
-                                    <li className={`ms-0 ${manu === '/shop-cart' ? 'active' : ''}`}><Link href="/shop-cart" className="sub-menu-item">Shop Cart</Link></li>
-                                    <li className={`ms-0 ${manu === '/shop-checkout' ? 'active' : ''}`}><Link href="/shop-checkout" className="sub-menu-item">Checkout</Link></li>
-                                    <li className={`ms-0 ${manu === '/our-store' ? 'active' : ''}`}><Link href="/our-store" className="sub-menu-item">Our Store</Link></li>
-                                    <li className={`ms-0 ${manu === '/brands' ? 'active' : ''}`}><Link href="/brands" className="sub-menu-item">Brands</Link></li>
-                                    <li className={`ms-0 ${manu === '/compare-product' ? 'active' : ''}`}><Link href="/compare-product" className="sub-menu-item">Compare Product</Link></li>
-                                    <li className={`ms-0 ${manu === '/recently-viewed-product' ? 'active' : ''}`}><Link href="/recently-viewed-product" className="sub-menu-item">Recently Viewed Products</Link></li>
                                 </ul>
                             </li>
-                
+
+                            <li className={`has-submenu parent-parent-menu-item ${['/shop-grid', '/shop-grid-left-sidebar', '/shop-grid-right-sidebar','/shop-list','/shop-list-left-sidebar','/shop-list-right-sidebar','/product-detail-one','/product-detail-two','/product-detail-three','/shop-cart','/shop-checkout','/our-store','/brands','/compare-product','/recently-viewed-product','/shop-item','/list-item','/detail-item','/grid-item'].includes(manu) ? 'active' : '' }`}>
+                                <Link href="#" onClick={() => setSubManu(subManu === '/shop-item' ? '' : '/shop-item')}> Womens </Link><span className="menu-arrow"></span>
+                                <ul className={`submenu ${['/shop-grid', '/shop-grid-left-sidebar', '/shop-grid-right-sidebar','/shop-list','/shop-list-left-sidebar','/shop-list-right-sidebar','/product-detail-one','/product-detail-two','/product-detail-three','/shop-cart','/shop-checkout','/our-store','/brands','/compare-product','/recently-viewed-product','/shop-item','/list-item','/detail-item','/grid-item'].includes(subManu) ? 'open' : ''}`}>
+                                    <li className={`has-submenu parent-menu-item ms-0 ${['/shop-grid','/shop-grid-left-sidebar','/shop-grid-right-sidebar','/grid-item'].includes(manu) ? 'active' : ''}`}>
+                                       <Link href="#" onClick={() => setSubManu(setManu === '/grid-item' ? '' : '/grid-item')}> Tops </Link>
+                                        <Link href="#" onClick={() => setSubManu(setManu === '/grid-item' ? '' : '/grid-item')}> Tops </Link>
+                                         <Link href="#" onClick={() => setSubManu(setManu === '/grid-item' ? '' : '/grid-item')}> Jeans </Link>
+                                         <Link href="#" onClick={() => setSubManu(setManu === '/grid-item' ? '' : '/grid-item')}> Pants </Link>
+                                    </li>
+                                </ul>
+                            </li>
+
                         <li className={`has-submenu parent-parent-menu-item ${['/aboutus','/user-account','/user-billing','/user-payment','/user-invoice','/user-social','/user-notification','/user-setting','/page-item','/user-item','/email-item','/email-confirmation','/email-cart','/email-offers','/email-order-success','/email-gift-voucher','/email-reset-password','/email-item-review','/blog-item','/blogs','/blog-detail','/help-item','/helpcenter','/helpcenter-faqs','/helpcenter-guides','/helpcenter-support','/auth-item','/login','/signup','/forgot-password','/lock-screen','/utility-item','/terms','/privacy','/comingsoon','/maintenance','/error','/special-item','/multi-item','/multi-item2','/multi-item3','/career'].includes(manu) ? 'active' : ''}`}>
-                            <Link href="#" onClick={()=>setSubManu(setManu === '/page-item' ? '' : '/page-item' )}>Pages</Link><span className="menu-arrow"></span>
+                            <Link href="#" onClick={()=>setSubManu(setManu === '/page-item' ? '' : '/page-item' )}>Account</Link><span className="menu-arrow"></span>
                             <ul className={`submenu ${['/aboutus','/user-account','/user-billing','/user-payment','/user-invoice','/user-social','/user-notification','/user-setting','/page-item','/user-item','/email-item','/email-confirmation','/email-cart','/email-offers','/email-order-success','/email-gift-voucher','/email-reset-password','/email-item-review','/blog-item','/blogs','/blog-detail','/help-item','/helpcenter','/helpcenter-faqs','/helpcenter-guides','/helpcenter-support','/auth-item','/login','/signup','/forgot-password','/lock-screen','/utility-item','/terms','/privacy','/comingsoon','/maintenance','/error','/special-item','/multi-item','/multi-item2','/multi-item3','/career'].includes(subManu) ? 'open' : ''}`}>
                                 <li className={`ms-0 ${manu === '/aboutus' ? 'active' : ''}`}><Link href="/aboutus" className="sub-menu-item">About Us</Link></li>
 
@@ -430,7 +342,7 @@ export default function Navbar({navClass, navlight}){
                                 </li> 
                             </ul>
                         </li>
-                            <li className={`${manu === '/sale' ? 'active' : ''}`}><Link href="/pages/sale" className="sub-menu-item">Sale</Link></li>
+                            {/* <li className={`${manu === '/sale' ? 'active' : ''}`}><Link href="/pages/sale" className="sub-menu-item">Sale</Link></li> */}
 
                             <li className={`${manu === '/contact' ? 'active' : ''}`}><Link href="/pages/contact" className="sub-menu-item">Contact</Link></li>
 
